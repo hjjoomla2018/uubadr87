695728f9e5fe75f3b99efafaf476901daf5885f1
Hamlet <hmoralesluis@gmail.com>
2018-08-30 19:06:39 -0400

    upload app files

---
+++ D_/Sites/PROJECTS/Boozka.Joomla/administrator/logs/error.php
@@ -0,0 +1,1 @@
+2018-08-20T14:51:33+00:00	INFO 89.10.167.14	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-08-09 16:52:19 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-08-09T16:52:19+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
